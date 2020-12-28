eb5fa08f3dd6e01cc193f8b050b69e2f53acbe9f
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2020-12-28 10:06:06 +0300

    header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -6,10 +6,22 @@ import {Info} from "../info/Info"
 export function Header() {
   return (
     <div className={style.header}>
-      {/* <div>className={style.toggleHolder}
-         <div id={style.toggle}></div>
-      </div> */}
-      <Info />
+      
+      <div className={style.toggleHolder}>
+         <div id={style.toggle}>
+           <div className={style.menuLine}></div>  
+         </div>
+      </div>
+
+      <div className={style.topPagination}>
+        <div className={style.currentPageNum}>01</div>
+        <div className={style.pagination}></div>
+        <div className={style.totalPageNum}>04</div>
+      </div>
+      <div className={style.myInfo}>
+        <Info />
+      </div>
+      
     </div>
   )
 }
